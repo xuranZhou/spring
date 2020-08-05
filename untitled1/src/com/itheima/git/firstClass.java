2ff5a98de5e8f453582786b9b52b5944b46d8903
xuran.zhou <xuran.zhou@pcitc.com>
2020-08-05 21:24:56 +0800

    添加新的程序

--- untitled1/src/com/itheima/git/firstClass.java
+++ untitled1/src/com/itheima/git/firstClass.java
@@ -4,4 +4,9 @@ public class firstClass {
     public static void main(String[] args) {
         System.out.println("hello git");
     }
+    public void test() {
+        int i=0;
+        i+=10;
+        System.out.println(i);
+    }
 }
